780dee7fe59cd617fb0a7382e97f88fcffc5454c
Pierre Glaser <pierreglaser@msn.com>
2018-09-17 11:53:33 +0200

    changed the dynamic_modules_globals test
    for now only a function of a dynamic module is unpickled,
    to show a case where creating a dynamic module is hard.

--- tests/cloudpickle_test.py
+++ tests/cloudpickle_test.py
@@ -442,7 +442,7 @@ class CloudPickleTest(unittest.TestCase):
         mod1, mod2 = pickle_depickle([mod, mod])
         self.assertEqual(id(mod1), id(mod2))
 
-    def test_dynamic_modules_cache(self):
+    def test_dynamic_modules_globals(self):
         # _dynamic_modules_globals is a WeakValueDictionary, so if this dynamic
         # module has no other reference than in this # dict (whether on the
         # child process or the parent process), this module will be garbage
@@ -452,23 +452,37 @@ class CloudPickleTest(unittest.TestCase):
         mod = imp.new_module('mod')
         code = '''
         x = 1
-        def f():
+        def func():
             return
         '''
         exec(textwrap.dedent(code), mod.__dict__)
 
         pickled_module_path = 'mod.pkl'
+        print(mod.func.__module__)
 
         with open(pickled_module_path, 'wb') as f:
-            cloudpickle.dump(mod, f)
+            cloudpickle.dump(mod.func, f)
 
         # _dynamic_modules_globals will have mod appended to its values
         # in the child process only
         child_process_script = '''
         import pickle
-        import cloudpickle
+        from cloudpickle.cloudpickle import _dynamic_modules_globals
+        import gc
         with open("{pickled_module_path}", 'rb') as f:
-            _ = pickle.load(f)
+            func = pickle.load(f)
+
+        # A dictionnary storing the globals of the newly unpickled function
+        # should have been created
+        assert list(_dynamic_modules_globals.keys())==['mod']
+
+        del func
+        gc.collect()
+
+        # There is no reference to the globals of func since func has been
+        # deleted and _dynamic_modules_globals is a WeakValueDictionary,
+        # so _dynamic_modules_globals should now be empty
+        assert list(_dynamic_modules_globals.keys())==[]
         '''
         child_process_script = child_process_script.format(
                 pickled_module_path=pickled_module_path)
@@ -477,12 +491,6 @@ class CloudPickleTest(unittest.TestCase):
 
         # We remove the other references to the object and trigger a
         # gc event
-        del mod
-        gc.collect()
-
-        # At this point, there is no other reference to this module, so
-        # _dynamic_modules_globals should be empty
-        assert list(_dynamic_modules_globals.keys()) == []
 
     def test_load_dynamic_module_in_grandchild_process(self):
         # Make sure that when loaded, a dynamic module preserves its dynamic
