795885d3ed9c050934746ee9b3a1f1068c020902
Olivier Grisel <olivier.grisel@ensta.org>
2019-01-23 17:34:57 +0100

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import re
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+
+# Function to parse __version__ in `cloudpickle/__init__.py`
+def find_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'cloudpickle', '__init__.py'), 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 dist = setup(
     name='cloudpickle',
-    version='0.6.1',
+    version=find_version(),
     description='Extended pickling support for Python objects',
     author='Cloudpipe',
     author_email='cloudpipe@googlegroups.com',
