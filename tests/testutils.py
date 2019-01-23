a9d2c8b5d270baccae6aa26411fdf5cef38b3e2b
Olivier Grisel <olivier.grisel@ensta.org>
2019-01-23 14:26:13 +0100

    try to workaround empty stdin on windows with Python 2.7

--- tests/testutils.py
+++ tests/testutils.py
@@ -56,7 +56,8 @@ def subprocess_pickle_echo(input_data, protocol=None):
     # run then pickle_echo(protocol=protocol) in __main__:
     cmd = [sys.executable, __file__, "--protocol", str(protocol)]
     cwd, env = _make_cwd_env()
-    proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=cwd, env=env)
+    proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=cwd, env=env,
+                 shell=True)
     try:
         comm_kwargs = {}
         if timeout_supported:
