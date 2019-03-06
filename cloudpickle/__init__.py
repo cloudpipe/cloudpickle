7da9aafc610a63cc76c036b0fb0e17a9f740f92e
Pierre Glaser <pierreglaser@msn.com>
2019-03-06 15:39:14 +0100

    MNT load cloudpickle_fast for recent (>3.8) python

--- cloudpickle/__init__.py
+++ cloudpickle/__init__.py
@@ -1,5 +1,11 @@
 from __future__ import absolute_import
 
-from cloudpickle.cloudpickle_fast import *
+import sys
+
+
+if sys.version_info[:2] >= (3, 8):
+    from cloudpickle.cloudpickle_fast import *
+else:
+    from cloudpickle.cloudpickle import *
 
 __version__ = '1.2.0.dev0'
