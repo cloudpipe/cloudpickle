cb78fc92e1ffdf633dd66b5ae99017ae8199317f
Pierre Glaser <pierreglaser@msn.com>
2020-02-03 16:25:57 +0100

    debug distributed remotely using my fork

--- .github/scripts/install_downstream_project.sh
+++ .github/scripts/install_downstream_project.sh
@@ -2,5 +2,10 @@ python -m pip install $TEST_REQUIREMENTS
 python -m pip uninstall -y cryptography
 pushd ..
 git clone $PROJECT_URL;
+
+pushd $PROJECT
+git checkout debug-test-remote-access
+popd
+
 python -m pip install ./$PROJECT;
 popd;
