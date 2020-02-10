356e37a426aeaffad1d6b9382dfa51c50bf9669d
Pierre Glaser <pierreglaser@msn.com>
2020-02-10 15:15:52 +0100

    release 1.3.0

--- cloudpickle/__init__.py
+++ cloudpickle/__init__.py
@@ -8,4 +8,4 @@ from cloudpickle.cloudpickle import *
 if sys.version_info[:2] >= (3, 8):
     from cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump
 
-__version__ = '1.3.0dev0'
+__version__ = '1.3.0'
