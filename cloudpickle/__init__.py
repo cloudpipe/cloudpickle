74487956582506ef323e705d4bb06ab16ece3b7a
John Kirkham <jakirkham@gmail.com>
2020-05-07 02:19:29 -0700

    Use `cloudpickle_fast` if protocol 5 is used

--- cloudpickle/__init__.py
+++ cloudpickle/__init__.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
-import sys
 import pickle
 
 
 from cloudpickle.cloudpickle import *
-if sys.version_info[:2] >= (3, 8):
+if pickle.HIGHEST_PROTOCOL >= 5:
     from cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump
 
 __version__ = '1.5.0dev0'
