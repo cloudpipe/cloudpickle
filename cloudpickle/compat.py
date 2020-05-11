a8da6e32a4c5bfc4855728be06c99fe588a5783c
John Kirkham <jakirkham@gmail.com>
2020-05-10 20:57:24 -0700

    Ignore unused imports in `compat` module

--- cloudpickle/compat.py
+++ cloudpickle/compat.py
@@ -3,11 +3,11 @@ import sys
 
 if sys.version_info.major == 3 and sys.version_info.minor < 8:
     try:
-        import pickle5 as pickle
-        import pickle5._pickle as _pickle
+        import pickle5 as pickle  # noqa: F401
+        import pickle5._pickle as _pickle  # noqa: F401
     except ImportError:
-        import pickle
-        import _pickle
+        import pickle  # noqa: F401
+        import _pickle  # noqa: F401
 else:
-    import pickle
-    import _pickle
+    import pickle  # noqa: F401
+    import _pickle  # noqa: F401
