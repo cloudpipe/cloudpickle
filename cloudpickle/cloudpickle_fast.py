3b5ec5d02d56fe5e03a270a13e107dadf92d9709
John Kirkham <jakirkham@gmail.com>
2020-05-10 21:09:06 -0700

    Only handle `CellType` when available
    As `CellType` was added in Python 3.8, it won't be available in Python
    3.6 or 3.7. So check before adding it to the dispatch table.

--- cloudpickle/cloudpickle_fast.py
+++ cloudpickle/cloudpickle_fast.py
@@ -415,7 +415,8 @@ class CloudPickler(Pickler):
     dispatch[memoryview] = _memoryview_reduce
     dispatch[property] = _property_reduce
     dispatch[staticmethod] = _classmethod_reduce
-    dispatch[types.CellType] = _cell_reduce
+    if hasattr(types, "CellType"):
+        dispatch[types.CellType] = _cell_reduce
     dispatch[types.CodeType] = _code_reduce
     dispatch[types.GetSetDescriptorType] = _getset_descriptor_reduce
     dispatch[types.ModuleType] = _module_reduce
