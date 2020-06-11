5bfb9f3739c11a6dcfb38016c90f2d9a265aaa46
John Kirkham <jakirkham@gmail.com>
2020-06-10 17:48:35 -0700

    Tweak pickle5 imports

--- cloudpickle/compat.py
+++ cloudpickle/compat.py
@@ -4,8 +4,7 @@ import sys
 if sys.version_info < (3, 8):
     try:
         import pickle5 as pickle  # noqa: F401
-        import pickle5._pickle as _pickle  # noqa: F401
-        from pickle5._pickle import Pickler  # noqa: F401
+        from pickle5 import _Pickler as Pickler  # noqa: F401
     except ImportError:
         import pickle  # noqa: F401
         from pickle import _Pickler as Pickler  # noqa: F401
