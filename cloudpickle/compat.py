6530ff639680c21a720e998e29ce1e8f5882d293
Pierre Glaser <pierreglaser@msn.com>
2020-06-30 20:12:50 +0200

    use C-implemented pickler if pickle5 is installed

--- cloudpickle/compat.py
+++ cloudpickle/compat.py
@@ -4,7 +4,7 @@ import sys
 if sys.version_info < (3, 8):
     try:
         import pickle5 as pickle  # noqa: F401
-        from pickle5 import _Pickler as Pickler  # noqa: F401
+        from pickle5 import Pickler  # noqa: F401
     except ImportError:
         import pickle  # noqa: F401
         from pickle import _Pickler as Pickler  # noqa: F401
