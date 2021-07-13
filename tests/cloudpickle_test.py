ad85452e832ae89db7863fc28d2e1980cff4e559
Pierre Glaser <pierreglaser@msn.com>
2021-07-14 00:22:05 +0100

    CI again

--- tests/cloudpickle_test.py
+++ tests/cloudpickle_test.py
@@ -2381,7 +2381,7 @@ class CloudPickleTest(unittest.TestCase):
                 w.run(
                     lambda p: sys.path.remove(p), _mock_interactive_session_cwd
                 )
-                raise ValueError(w.run(lambda p: sys.path))
+                raise ValueError(w.run(lambda: sys.path))
                 with pytest.raises(ImportError):
                     w.run(lambda: __import__("mock_local_file"))
 
