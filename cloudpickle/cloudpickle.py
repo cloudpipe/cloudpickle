5f079b0e29b34bd346759db2a73bcc9dcc7a3277
Pierre Glaser <pierreglaser@msn.com>
2021-07-31 17:14:17 +0100

    fix linting errors

--- cloudpickle/cloudpickle.py
+++ cloudpickle/cloudpickle.py
@@ -246,17 +246,17 @@ def _whichmodule(obj, name):
 
 def _should_pickle_by_reference(obj, name=None):
     """Test whether an function or a class should be pickled by reference
-    
-     Pickling by reference means by that the object (typically a function
-     or a class) is an attribute of a module that is assumed to be importable
-     in the target Python environment. Loading will therefore rely on importing
-     the module and then calling `getattr` on it to access the function or class.
-     
-     Pickling by reference is the only option to pickle functions and classes in the
-     standard library. In cloudpickle the alternative option is to pickle by value (for
-     instance for interactively or locally defined functions and classes or for
-     attributes of modules that have been explicitly registered to be pickled by
-     value.
+
+     Pickling by reference means by that the object (typically a function or a
+     class) is an attribute of a module that is assumed to be importable in the
+     target Python environment. Loading will therefore rely on importing the
+     module and then calling `getattr` on it to access the function or class.
+
+     Pickling by reference is the only option to pickle functions and classes
+     in the standard library. In cloudpickle the alternative option is to
+     pickle by value (for instance for interactively or locally defined
+     functions and classes or for attributes of modules that have been
+     explicitly registered to be pickled by value.
      """
     if isinstance(obj, types.FunctionType) or issubclass(type(obj), type):
         module_and_name = _lookup_module_and_qualname(obj, name=name)
