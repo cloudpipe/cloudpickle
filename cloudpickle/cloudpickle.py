0589dcba6456f72e92264556c195a0c96befe2f3
Pierre Glaser <pierreglaser@msn.com>
2021-07-31 17:15:26 +0100

    more linting...

--- cloudpickle/cloudpickle.py
+++ cloudpickle/cloudpickle.py
@@ -129,20 +129,20 @@ def _lookup_class_or_track(class_tracker_id, class_def):
 
 def register_pickle_by_value(module):
     """Register a module to make it functions and classes picklable by value.
-    
+
     By default, functions and classes that are attributes of an importable
     module are to be pickled by reference, that is relying on re-importing
     the attribute from the module at load time.
-    
+
     If `register_pickle_by_value(module)` is called, all its functions and
     classes are subsequently to be pickled by value, meaning that they can
     be loaded in Python processes where the module is not importable.
-    
+
     This is especially useful when developing a module in a distributed
     execution environment: restarting the client Python process with the new
     source code is enough: there is no need to re-install the new version
     of the module on all the worker nodes nor to restart the workers.
-    
+
     Note: this feature is considered experimental. See the cloudpickle
     README.md file for more details and limitations.
     """
