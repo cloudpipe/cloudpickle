bd57733a4852727dd2437abc500afab457a6c992
Pierre Glaser <pierreglaser@msn.com>
2021-09-08 20:08:01 +0100

    cd in the right place

--- .github/scripts/install_downstream_project.sh
+++ .github/scripts/install_downstream_project.sh
@@ -1,6 +1,8 @@
 python -m pip install $TEST_REQUIREMENTS
 pushd ..
-git clone $PROJECT_URL;
+git clone $PROJECT_URL
+pushd $PROJECT
 git checkout 53fcdbbcd9d3ba1bf96fb2075f92fb1925e2584d
+popd
 python -m pip install ./$PROJECT;
 popd;
