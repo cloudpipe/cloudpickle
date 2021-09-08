5e27b1100e42c800ea92f510beb14355e21d77ef
Pierre Glaser <pierreglaser@msn.com>
2021-09-08 21:17:54 +0100

    isolate problematic test

--- .github/scripts/test_downstream_project.sh
+++ .github/scripts/test_downstream_project.sh
@@ -1,5 +1,5 @@
 pushd ../$PROJECT
-python -m pytest -vl $ADDITIONAL_PYTEST_ARGS
+python -m pytest -vl $ADDITIONAL_PYTEST_ARGS -k "test_pickle_empty"
 TEST_RETURN_CODE=$?
 popd
 if [[ "$TEST_RETURN_CODE" != "0" ]]; then
