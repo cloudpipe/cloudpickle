0e8c7a44b27313f7891975076c0dfac5e0b48840
Pierre Glaser <pierreglaser@msn.com>
2021-09-08 20:05:39 +0100

    install last relase and not master

--- .github/scripts/install_downstream_project.sh
+++ .github/scripts/install_downstream_project.sh
@@ -1,4 +1,6 @@
 python -m pip install $TEST_REQUIREMENTS
 pushd ..
-python -m pip install $PROJECT;
+git clone $PROJECT_URL;
+git checkout 53fcdbbcd9d3ba1bf96fb2075f92fb1925e2584d
+python -m pip install ./$PROJECT;
 popd;
