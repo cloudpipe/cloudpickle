93018ce76c21a1f3ed7dc400b5f7294bd724dfe4
Pierre Glaser <pierreglaser@msn.com>
2021-09-08 20:12:21 +0100

    bisect manually

--- .github/scripts/install_downstream_project.sh
+++ .github/scripts/install_downstream_project.sh
@@ -2,7 +2,7 @@ python -m pip install $TEST_REQUIREMENTS
 pushd ..
 git clone $PROJECT_URL
 pushd $PROJECT
-git checkout 53fcdbbcd9d3ba1bf96fb2075f92fb1925e2584d
+git checkout 047b6eff2240c918cbc15282aad2241b6a0b612f 
 popd
 python -m pip install ./$PROJECT;
 popd;
