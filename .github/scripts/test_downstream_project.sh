5b671562cdab8c2ade4568210479e3ceb82f766a
Pierre Glaser <pierreglaser@msn.com>
2021-09-09 20:27:03 +0100

    debug

--- .github/scripts/test_downstream_project.sh
+++ .github/scripts/test_downstream_project.sh
@@ -1,8 +1,9 @@
 pushd ../$PROJECT
+echo "${DISABLE_IPV6}"
 echo "${PYTEST_ADDOPTS[@]}"
-python -m pytest -vl "${PYTEST_ADDOPTS[@]}"
-TEST_RETURN_CODE=$?
-popd
-if [[ "$TEST_RETURN_CODE" != "0" ]]; then
-    exit $TEST_RETURN_CODE
-fi
+# python -m pytest -vl "${PYTEST_ADDOPTS[@]}"
+# TEST_RETURN_CODE=$?
+# popd
+# if [[ "$TEST_RETURN_CODE" != "0" ]]; then
+#     exit $TEST_RETURN_CODE
+# fi
