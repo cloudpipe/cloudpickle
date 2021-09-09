632a01de6961f8fd8201a5e667f0b6e470773f36
Pierre Glaser <pierreglaser@msn.com>
2021-09-09 18:12:17 +0100

    test pickle related tests

--- .github/scripts/test_downstream_project.sh
+++ .github/scripts/test_downstream_project.sh
@@ -1,5 +1,5 @@
 pushd ../$PROJECT
-python -m pytest -vl $ADDITIONAL_PYTEST_ARGS -k "test_pickle_empty"
+python -m pytest -vl $ADDITIONAL_PYTEST_ARGS -k "pickle or serializ"
 TEST_RETURN_CODE=$?
 popd
 if [[ "$TEST_RETURN_CODE" != "0" ]]; then
