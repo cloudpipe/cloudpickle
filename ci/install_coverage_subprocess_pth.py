126cf6ded015f2db03d49b4ab7dc65974d39f29e
Ben Beasley <code@musicinmybrain.net>
2023-07-03 11:54:26 -0400

    Handle removal of distutils from stdlib in Python 3.12 (#508)
    Use setuptools as a replacement for now.

--- ci/install_coverage_subprocess_pth.py
+++ ci/install_coverage_subprocess_pth.py
@@ -3,13 +3,13 @@
 # http://coverage.readthedocs.io/en/latest/subprocess.html
 
 import os.path as op
-from distutils.sysconfig import get_python_lib
+from sysconfig import get_path
 
 FILE_CONTENT = u"""\
 import coverage; coverage.process_startup()
 """
 
-filename = op.join(get_python_lib(), 'coverage_subprocess.pth')
+filename = op.join(get_path('purelib'), 'coverage_subprocess.pth')
 with open(filename, 'wb') as f:
     f.write(FILE_CONTENT.encode('ascii'))
 
