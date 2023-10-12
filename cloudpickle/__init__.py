c9f4b6e5e9e241c349522e0bd9d2ea8bef44d7cb
Olivier Grisel <olivier.grisel@ensta.org>
2023-10-12 17:23:16 +0200

    Forgot about register_pickle_by_value/unregister_pickle_by_value which is also part of the public API

--- cloudpickle/__init__.py
+++ cloudpickle/__init__.py
@@ -1,7 +1,15 @@
 from cloudpickle.cloudpickle import *  # noqa
 
-__version__ = '3.0.0.dev0'
+__version__ = "3.0.0.dev0"
 
-__all__ = [ # noqa
-    "__version__", "Pickler", "CloudPickler", "dumps", "loads", "dump", "load"
+__all__ = [  # noqa
+    "__version__",
+    "Pickler",
+    "CloudPickler",
+    "dumps",
+    "loads",
+    "dump",
+    "load",
+    "register_pickle_by_value",
+    "unregister_pickle_by_value",
 ]
