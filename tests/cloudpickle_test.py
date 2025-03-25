543ab5928282958bf9206f9ba2c51e928e932570
Thomas Moreau <thomas.moreau.2010@gmail.com>
2025-03-25 10:32:53 +0100

    MTN skip test with recursion limit on python3.14 due to segfault on OSX (#561)

--- tests/cloudpickle_test.py
+++ tests/cloudpickle_test.py
@@ -2507,12 +2507,18 @@ class CloudPickleTest(unittest.TestCase):
         inner_func = depickled_factory()
         assert inner_func() == _TEST_GLOBAL_VARIABLE
 
+    # TODO: remove this xfail when we drop support for Python 3.8. We don't
+    # plan to fix it because Python 3.8 is EOL.
     @pytest.mark.skipif(
         sys.version_info < (3, 9),
         reason="Can cause CPython 3.8 to segfault",
     )
-    # TODO: remove this xfail when we drop support for Python 3.8. We don't
-    # plan to fix it because Python 3.8 is EOL.
+    @pytest.mark.skipif(
+        sys.version_info > (3, 14),
+        reason="Can cause CPython 3.14 interpreter to crash",
+        # This interpreter crash is reported upstream in
+        # https://github.com/python/cpython/issues/131543
+    )
     def test_recursion_during_pickling(self):
         class A:
             def __getattribute__(self, name):
